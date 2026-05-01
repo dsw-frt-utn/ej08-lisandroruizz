a686d6b82e87bf95b473eace52d2ccca23f806bc
Lisandro Ruiz <lisandroruiz05@gmail.com>
2026-05-01 20:03:03 -0300

    Problema N2 agregado, resumen con clase anonima

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -11,6 +11,14 @@ namespace Dsw2026Ej8
 
             string etiqueta = helper.ObtenerEtiquetaProducto(1001, "Mouse Logitech", 2500);
 
+            Problema2 problema2 = new Problema2();
+
+            string resumen1 = problema2.CrearResumenVenta(1001, "Teclado", 3, 1500);
+            string resumen2 = problema2.CrearResumenVenta(1002, "Mouse", 0, 2500);
+
+            Console.WriteLine(resumen1);
+            Console.WriteLine(resumen2);
+
             Console.WriteLine(etiqueta);
         }
     }
