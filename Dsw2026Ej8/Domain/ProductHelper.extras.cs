aab89125fc5617017ca4df5194504459c9b3c039
Lisandro Ruiz <lisandroruiz05@gmail.com>
2026-05-01 19:51:20 -0300

    Problema N1, Clase ProductHelper partial agregada

---
+++ Dsw2026Ej8/Domain/ProductHelper.extras.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Domain
+{
+    public partial class ProductHelper
+    {
+        private string FormatearMoneda(decimal price)
+        {
+            return $"${price:N2}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Dsw2026Ej8
-{
+﻿using Dsw2026Ej8.Domain;
+namespace Dsw2026Ej8
+
+{ 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            ProductHelper helper = new ProductHelper();
+
+            string etiqueta = helper.ObtenerEtiquetaProducto(1001, "Mouse Logitech", 2500);
+
+            Console.WriteLine(etiqueta);
         }
     }
 }
